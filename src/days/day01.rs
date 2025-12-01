7fd962f57b95e1d58f0443881334b1a033114c5a
DuckySmacky <kriperoka@gmail.com>
2025-12-01 17:04:28 +0300

    Finish day 1 part 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -4,19 +4,51 @@ use crate::days::DaySolution;
 pub struct Solution {}
 
 impl DaySolution for Solution {
-  type Output = String;
+  type Output = i64;
 
   fn day_number(&self) -> u8 {
     1
   }
 
   fn part1(&self, input: Vec<String>) -> Result<Self::Output, String> {
-    input.iter().for_each(|s| println!("{}", s));
-    Ok("Day 1 part 1 placeholder answer".to_string())
+    let mut password = 0;
+
+    input.into_iter()
+      .fold(50, |dial, line| {
+        let dial = turn_dial(dial, &line);
+        if dial == 0 { password += 1; }
+        dial
+      });
+
+    Ok(password)
   }
 
   fn part2(&self, input: Vec<String>) -> Result<Self::Output, String> {
-    input.iter().for_each(|s| println!("{}", s));
-    Ok("Day 1 part 2 placeholder answer".to_string())
+    
+
+    Ok(0)
   }
+}
+
+fn turn_dial(dial: i64, rotation: &str) -> i64 {
+    let side = rotation.chars().nth(0).unwrap();
+    let amount = rotation[1..].parse::<i64>().unwrap();
+
+    let dial = match side {
+        'L' => dial - amount,
+        'R' => dial + amount,
+        _ => 0,
+    } as i64;
+
+    normalize_dial(dial)
+}
+
+fn normalize_dial(dial: i64) -> i64 {
+    if dial < 0 {
+        normalize_dial(99 + dial + 1)
+    } else if dial > 99 {
+        normalize_dial(dial - 99 - 1)
+    } else {
+        dial as i64
+    }
 }
