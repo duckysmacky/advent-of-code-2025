9c51d35207cd5e2232b7ea485af2a24fc34eb609
DuckySmacky <kriperoka@gmail.com>
2025-12-03 16:04:58 +0300

    Finish day 2 part 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -43,6 +43,54 @@ impl DaySolution for Solution {
     }
 
     fn part2(&self, input: Vec<String>) -> Result<Self::Output, String> {
-        todo!()
+        let line = input.get(0).unwrap();
+
+        let sum = line.split(",")
+            .fold(0u64, |mut sum, range| {
+                let mut segments = range.split("-");
+                let start = segments.next().unwrap().parse::<u64>().unwrap();
+                let end = segments.next().unwrap().parse::<u64>().unwrap();
+
+                for n in start..=end {
+                    let num = n.to_string();
+
+                    if check_num(num) {
+                        sum += n
+                    }
+                }
+
+                sum
+            });
+
+        Ok(sum)
     }
+}
+
+fn check_num(num: String) -> bool {
+    let len = num.len();
+    let mut cut_size = len / 2;
+
+    'cut: while cut_size > 0 {
+        let cut = &num[..cut_size];
+
+        if len % cut_size != 0 {
+            cut_size -= 1;
+            continue;
+        }
+
+        for i in 1..len / cut_size {
+            let from = cut_size * i;
+            let to = from + cut_size;
+            let curr_cut = &num[from..to];
+
+            if curr_cut != cut {
+                cut_size -= 1;
+                continue 'cut;
+            }
+        }
+
+        return true;
+    }
+
+    false
 }
