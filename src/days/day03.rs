49b496bb790db5891e1f036d7d5f302ce54b071b
DuckySmacky <kriperoka@gmail.com>
2025-12-05 01:00:37 +0300

    Finish day 3 part 2

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -4,7 +4,7 @@ use crate::days::DaySolution;
 pub struct Solution {}
 
 impl DaySolution for Solution {
-    type Output = u32;
+    type Output = u64;
 
     fn day_number(&self) -> u8 {
         3
@@ -33,11 +33,50 @@ impl DaySolution for Solution {
                 total + best_num
             });
 
-        Ok(total)
+        Ok(total as u64)
     }
 
     fn part2(&self, input: Vec<String>) -> Result<Self::Output, String> {
-        Ok(0)
+        let total = input.into_iter()
+            .fold(0u64, |total, line| {
+                let nums = line.chars().into_iter()
+                    .map(|c| c.to_digit(10).unwrap() as u8)
+                    .collect::<Vec<_>>();
+
+                let mut num_digits = [0u8; 12];
+
+                let mut digit_i = 0;
+                let mut start_i = 0;
+                for num_i in (nums.len() - 12)..nums.len() {
+                    let mut max_num = nums[num_i];
+                    let mut found_max = false;
+
+                    for max_i in start_i..num_i {
+                        if nums[max_i] > max_num || (nums[max_i] == max_num && !found_max) {
+                            max_num = nums[max_i];
+                            start_i = max_i + 1;
+                            found_max = true;
+                        }
+                    }
+
+                    if start_i <= num_i && !found_max {
+                        start_i = num_i + 1;
+                    }
+
+                    num_digits[digit_i] = max_num;
+                    digit_i += 1;
+                }
+
+                let num = num_digits.into_iter()
+                    .enumerate()
+                    .fold(0, |num, (i, val)| {
+                        num + 10u64.pow(12 - i as u32 - 1) * val as u64
+                    });
+
+                total + num
+            });
+
+        Ok(total)
     }
 }
 
