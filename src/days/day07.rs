482ef16c017c3849ae4a7ccff914a15a5828a0a4
nikdor <kriperoka@gmail.com>
2025-12-07 16:20:40 +0300

    Finish day 7 part 2

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,10 +1,12 @@
+use std::collections::HashMap;
+
 use crate::days::DaySolution;
 
 #[derive(Default)]
 pub struct Solution {}
 
 impl DaySolution for Solution {
-    type Output = u32;
+    type Output = u64;
 
     fn day_number(&self) -> u8 {
         7
@@ -45,7 +47,35 @@ impl DaySolution for Solution {
         Ok(count)
     }
 
-    fn part2(&self, _input: Vec<String>) -> Result<Self::Output, String> {
-        Ok(0)
+    fn part2(&self, input: Vec<String>) -> Result<Self::Output, String> {
+        let start_col = input[0].find('S').unwrap();
+
+        let mut cache = HashMap::new();
+        let count = find(&mut cache, &input, 1, start_col);
+
+        Ok(count)
     }
+}
+
+fn find(cache: &mut HashMap<(usize, usize), u64>, manifold: &Vec<String>, row: usize, col: usize) -> u64 {
+    if row >= manifold.len() {
+        return 1;
+    }
+
+    if col >= manifold[row].len() {
+        return 0;
+    }
+
+    if let Some(&count) = cache.get(&(row, col)) {
+        return count;
+    }
+
+    let count = match manifold[row].as_bytes()[col] {
+        b'.' => find(cache, manifold, row + 1, col),
+        b'^' => find(cache, manifold, row + 1, col - 1) + find(cache, manifold, row + 1, col + 1),
+        _ => 0,
+    };
+
+    cache.insert((row, col), count);
+    count
 }
